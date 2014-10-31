bbba7491be83dcde78c1636e28782f336052743e
peter <phally1980@gmail.com>
2014-10-31 14:03:45 +0000

    defferal object containing program

--- src/main/java/com/citonline/domain/Deferral.java
+++ src/main/java/com/citonline/domain/Deferral.java
@@ -8,14 +8,38 @@ public class Deferral
 	Calendar defferalDate;
 	Student student;
 	ArrayList<Module> defferedModules;
+	boolean programDeffered;
+	Program program;
+	
+	
 	
 	public Deferral(Calendar defferalDate, Student student,
-			ArrayList<Module> defferedModules) {
+			ArrayList<Module> defferedModules, boolean programDeffered,
+			Program program) {
 		super();
 		this.defferalDate = defferalDate;
 		this.student = student;
 		this.defferedModules = defferedModules;
+		this.programDeffered = programDeffered;
+		this.program = program;
 	}
+
+	public boolean isProgramDeffered() {
+		return programDeffered;
+	}
+
+	public void setProgramDeffered(boolean programDeffered) {
+		this.programDeffered = programDeffered;
+	}
+
+	public Program getProgram() {
+		return program;
+	}
+
+	public void setProgram(Program program) {
+		this.program = program;
+	}
+
 	
 	public Calendar getDefferalDate() {
 		return defferalDate;
