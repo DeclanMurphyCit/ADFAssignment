728ca7d7c47450b893e15203696696e1632a754d
Declan <Declan@DECLAN-LAPTOP>
2014-11-03 18:04:02 +0000

    updated student dao & jdbc template

--- src/main/java/com/citonline/db/StudentJdbcTemplate.java
+++ src/main/java/com/citonline/db/StudentJdbcTemplate.java
@@ -1,20 +1,24 @@
 package com.citonline.db;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-import com.citonline.domain.Lecturer;
 import com.citonline.domain.Student;
 
 public class StudentJdbcTemplate implements StudentDAO {
 
+	@Autowired
 	private DataSource dataSource;
 	private JdbcTemplate jdbcTemplateObject;
 
-	@Override
+	@Autowired
 	public void setDataSource(DataSource dataSource) {
 		this.dataSource = dataSource;
 		this.jdbcTemplateObject = new JdbcTemplate(dataSource);
@@ -40,70 +44,121 @@ public class StudentJdbcTemplate implements StudentDAO {
 
 	@Override
 	public void deleteStudent(Integer id) {
-		String SQL = "delete from Student where id = ?";
+		String SQL = "delete from Student where id_student = ?";
 		jdbcTemplateObject.update(SQL, new Object[] {id});
-		System.out.println("Deleted student where id = " + id );		
+		System.out.println("Deleted student where id_student = " + id );		
 	}
 
 	@Override
 	public void deleteStudent(String studentNumber) {
 		String SQL = "delete from Student where studentNumber = ?";
 		jdbcTemplateObject.update(SQL, new Object[] {studentNumber});
-		System.out.println("Deleted student where student number = " + studentNumber );
+		System.out.println("Deleted student where studentNumber = " + studentNumber );
 		
 	}
 
 	@Override
 	public Student getStudent(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		String SQL = "select * from Student where id_student = ?";
+		Student student = (Student) jdbcTemplateObject.queryForObject(SQL, 
+						new Object[]{id}, new StudentMapper());
+		
+		return student;
 	}
 
 	@Override
 	public Student getStudent(String studentNumber) {
-		// TODO Auto-generated method stub
-		return null;
+		String SQL = "select * from Student where studentNumber = ?";
+		Student student = (Student) jdbcTemplateObject.queryForObject(SQL, 
+						new Object[]{studentNumber}, new StudentMapper());
+		
+		return student;
 	}
 
 	@Override
 	public List<Student> listStudents() {
-		// TODO Auto-generated method stub
-		return null;
+		String SQL = "select * from student";
+		List<Student> studentList = jdbcTemplateObject.query(SQL, 
+						new StudentMapper());
+		return studentList;
 	}
 
 	@Override
 	public void updateStudentEmail(String studentNumber, String email) {
-		// TODO Auto-generated method stub
-		
+		String SQL = "update student set email = ? where studentNumber = ?";
+		jdbcTemplateObject.update(SQL, new Object[] {email,studentNumber});
+		System.out.println("Updated student email to " + email + " where studentNumber = " + studentNumber );
 	}
 
 	@Override
 	public void updateStudentEmail(Integer id, String email) {
-		// TODO Auto-generated method stub
+		String SQL = "update student set email = ? where id_student = ?";
+		jdbcTemplateObject.update(SQL, new Object[] {email,id});
+		System.out.println("Updated student email to " + email + " where id_student = " + id );
+		
+	}
+	
+	@Override
+	public void updateStudentAddress(String studentNumber, String addressLine1, String addressLine2) {
+		String SQL = "update student set addressLine1 = ?, addressLine2 = ? where studentNumber = ?";
+		jdbcTemplateObject.update(SQL, new Object[] {addressLine1,addressLine1,studentNumber});
+		System.out.println("Updated student address to " + addressLine1 + ", "+addressLine1+ " where studentNumber = " + studentNumber );
 		
 	}
 
 	@Override
-	public void addProgram(Integer idProgram) {
-		// TODO Auto-generated method stub
-		
+	public void updateStudentAddress(Integer id, String addressLine1, String addressLine2) {
+		String SQL = "update student set email = ? where id_student = ?";
+		jdbcTemplateObject.update(SQL, new Object[] {addressLine1,addressLine1,id});
+		System.out.println("Updated student address to " + addressLine1 + ", "+addressLine1+ " where id_student = " + id );
 	}
 
 	@Override
-	public void addModule(Integer idModule) {
-		// TODO Auto-generated method stub
+	public void enrollModule(Integer idModule, Integer idStudent) {
+	
+		//String SQL = "SELECT id_student from student WHERE studentNumber = ?";
 		
+		String SQL = "INSERT into student_enrolls_for (idModule,idStudent) "
+				+ "values (?,?)";
+		jdbcTemplateObject.update(SQL, new Object[] {idModule,idStudent});
+		
+		System.out.println("Student "+idStudent + " has enrolled for module" + idModule);			
+	}
+	
+	@Override
+	public void enrollModules(final Integer idStudent, final List<Integer> idModuleList) {
+		String SQL = "INSERT INTO student_enrolls_for (idStudent,idModule)" +
+				"VALUES (?, ?)";
+		
+		System.out.println("Student "+idStudent + " has enrolled for modules: ");			
+
+		jdbcTemplateObject.update(SQL, new BatchPreparedStatementSetter() {
+
+			public int getBatchSize() {
+				return idModuleList.size();
+			}
+			
+			@Override
+			public void setValues(PreparedStatement ps, int i) throws SQLException {
+				Integer idModule = idModuleList.get(i);
+				ps.setInt(1, idStudent);
+				ps.setInt(2, idModule);
+				System.out.print(idModule + ",");
+			}		
+		});
+	}	
+
+	@Override
+	public void removeModule(Integer idStudent, Integer idModule) {
+		String SQL = "DELETE from student_enrolls_for WHERE idStudent = ? and idModule = ?";
+		jdbcTemplateObject.update(SQL, new Object[]{idStudent, idModule});
+		
+		System.out.println("Student " + idStudent + " has disenrolled from moudle: " + idModule);
 	}
 
 	@Override
 	public void addModuleDeferral(Integer idModule) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void addProgramDeferral(Integer idProgram) {
-		// TODO Auto-generated method stub
+		//????????????? Does this function belong here????????????????
 		
 	}
 
@@ -112,11 +167,4 @@ public class StudentJdbcTemplate implements StudentDAO {
 		// TODO Auto-generated method stub
 		
 	}
-
-	@Override
-	public void removeProgramDeferral(Integer idProgram) {
-		// TODO Auto-generated method stub
-		
-	}
-
 }
